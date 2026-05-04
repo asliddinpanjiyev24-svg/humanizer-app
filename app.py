d2f8debc1ecd76b6c84d5cb7bb33fedd19a45cbd
asliddinpanjiyev24-svg <asliddinpanjiyev24@gmail.com>
2026-05-05 01:29:07 +0500

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ if api_key:
 
     # Initialize the model
     model = genai.GenerativeModel(
-        model_name="gemini-1.5-flash",
+        model_name="gemini-pro",
         generation_config=generation_config,
     )
 
